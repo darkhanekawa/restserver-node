6e7e523ab43a140ea1bcbbcc4d0690fa00433b98
Kimberlyn Rodriguez <kimberl7n@hotmail.com>
2019-11-06 01:05:36 -0400

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://strider:EgJGboNYBRhuMYiD@cluster0-ytu5a.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
